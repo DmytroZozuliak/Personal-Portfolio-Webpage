6f7d633b9f6dcb291bd7f5fd5f556e7ee42ff848
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2021-06-18 12:36:49 +0300

    Personal Portfolio Webpage 1st edition

---
+++ js/app.js
@@ -0,0 +1,16 @@
+if (true) {
+  var x = 5;
+}
+console.log(x); // 5
+
+let y;
+if (true) {
+  let y = 15;
+}
+console.log(y); // undefined
+
+let z;
+if (true) {
+  z = 15;
+}
+console.log(z); // 15
---
+++ readme.md
@@ -0,0 +1,2 @@
+Personal Portfolio Webpage
+with link to another my works (codepen.io)
